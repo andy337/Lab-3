aa513433d77831ec24137a42dbd5ef105f21b908
andy337 <52741688+andy337@users.noreply.github.com>
2020-12-21 19:01:24 +0200

    Add files via upload

---
+++ Лаб 3/Лаб 3/MyFrac.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лаб_3
+{
+    class MyFrac
+    {
+        private long nom;
+        private long denom;
+
+        public MyFrac(long nom_, long denom_)
+        {
+            long a = nom_;
+            long b = denom_;
+            while (b != 0)
+                b = a % (a = b);
+
+            if (denom_ < 0)
+            {
+                denom_ = Math.Abs(denom_);
+                nom_ = -1 * nom_;
+                if (nom_ < 0)
+                {
+                    nom_ = Math.Abs(nom_);
+                }
+            }
+
+            nom = nom_ / Math.Abs(a);
+            denom = denom_ / Math.Abs(a);
+        }
+
+        public override string ToString()
+        {
+            return nom + "/" + denom;
+        }
+
+        public static MyFrac Plus(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom + f1.denom * f2.nom, f1.denom * f2.denom);
+        }
+
+        public static MyFrac Minus(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom - f2.nom * f1.denom, f1.denom * f2.denom);
+        }
+
+        public static MyFrac Multiply(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.nom, f1.denom * f2.denom);
+        }
+
+        public static MyFrac Divide(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom, f1.denom * f2.nom);
+        }
+
+        public static MyFrac GetRGR115LeftSum(int n)
+        {
+            MyFrac res = new MyFrac(1, 1);
+            for (int i = 2; i <= n; i++)
+            {
+                res = Multiply(res, Minus(new MyFrac(1, 1), new MyFrac(1, i * i)));
+            }
+            return res;
+        }
+
+        public static MyFrac GetRGR113LeftSum(int n)
+        {
+            MyFrac res = new MyFrac(0, 1);
+            for (int i = 1; i <= n; i++)
+            {
+                res = Plus(res, Multiply(new MyFrac(1, (2 * i) - 1), new MyFrac(1, (2 * i) + 1)));
+            }
+            return res;
+        }
+    }
+}
---
+++ Лаб 3/Лаб 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Лаб_3
+{
+    class Program
+    {
+        public static void InsertArray(ref double[][] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                array[i] = new double[rand.Next(1,10)];
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = rand.Next(1, 10);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
